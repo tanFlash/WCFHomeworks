63465a58eebbe79c86ab1adcc5682f9bc8aef9c1
tanFlash <mazurok.tania@gmail.com>
2015-02-18 21:23:39 +0200

    Homework 1 for WCF. Where we get information about particular drive

---
+++ Homework1/MyDisk/MyDisk/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using System.IO;
+
+namespace MyDisk
+{
+    [ServiceContract]
+    public interface IMyDiskInfo
+    {
+        [OperationContract]
+        string GetTotalSize(string str);
+
+        [OperationContract]
+        string GetFreeSpace(string str);
+    }
+
+    public class MyDiskInfo : IMyDiskInfo
+    {
+        public string GetTotalSize(string str)
+        {
+            //try
+            //{
+                string result;
+                DriveInfo drive = new DriveInfo((str[0]).ToString());
+                result = drive.TotalSize.ToString();
+                return result;
+            //}
+            //catch (Exception e)
+            //{
+            //    Console.WriteLine("There is no disk " + str[0]);
+            //    return str;
+            //}
+        }
+
+        public string GetFreeSpace(string str)
+        {
+            
+                string result;
+                DriveInfo drive = new DriveInfo((str[0]).ToString());
+                result = drive.TotalFreeSpace.ToString();
+                return result;
+            //}
+            //catch
+            //{
+            //    Console.WriteLine("There is no disk " + str[0]);
+            //    return str;
+            //}
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost sh = new ServiceHost(typeof(MyDiskInfo));
+            sh.AddServiceEndpoint(typeof(IMyDiskInfo), new WSHttpBinding(), "http://localhost:8001/MyMath/Ep1");
+            sh.Open();
+            Console.WriteLine("To end process press ENTER");
+            Console.ReadLine();
+            sh.Close();
+        }
+    }
+}
---
+++ Homework1/MyDiskClient/MyDiskClient/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+
+namespace MyDiskClient
+{
+    [ServiceContract]
+    public interface IMyDiskInfo
+    {
+        [OperationContract]
+        string GetTotalSize(string str);
+        [OperationContract]
+        string GetFreeSpace(string str);
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ChannelFactory<IMyDiskInfo> factory = new ChannelFactory<IMyDiskInfo>(
+               new WSHttpBinding(),
+               new EndpointAddress("http://localhost:8001/MyMath/Ep1"));
+            IMyDiskInfo channel = factory.CreateChannel();
+            Console.WriteLine("Write the name of the disk");
+            string str = Console.ReadLine();
+            string result = " ";
+            try
+            {
+                result = "Freespace = " + channel.GetFreeSpace(str).ToString() +
+                    "\tTotalspace= " + channel.GetTotalSize(str).ToString();
+            }
+            catch (Exception e)
+            {
+                result = "There is no disk " + str[0]; 
+            }
+            Console.WriteLine(result);
+            Console.WriteLine("Press ENTER to end the process");
+            Console.ReadLine();
+            factory.Close();
+        }
+    }
+}
