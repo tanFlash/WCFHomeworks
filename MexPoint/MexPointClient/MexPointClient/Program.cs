b19783631dcef2c8867f653b199d12d9a2ef79fb
tanFlash <mazurok.tania@gmail.com>
2015-03-01 14:49:31 +0200

    Task with mex point

---
+++ MexPoint/MexPointClient/MexPointClient/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MexPointClient.ServiceReference1;
+
+namespace MexPointClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyMathClient proxy = new MyMathClient("WSHttpBinding_IMyMath1");
+            IAsyncResult result2;
+            int result = proxy.Add(12, 2);
+            Console.WriteLine("Sum = " + result);
+            Console.WriteLine("To end press ENTER");
+           
+            
+            result2 = proxy.BeginWrite("Pupkin", "Vasyl", GetResultCallback, proxy);
+            
+            Console.ReadLine();
+        }
+        static void GetResultCallback(IAsyncResult ar)
+        {
+            Person p = ((MyMathClient)ar.AsyncState).EndWrite(ar);
+            Console.WriteLine("Name= " + p.FirstName + " Surname = " + p.LastName);
+        }
+    }
+}
---
+++ MexPoint/MexPointServer/MexPointServer/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+using System.Runtime.Serialization;
+
+namespace MexPointServer
+{
+    [DataContract]
+    public class Person
+    {
+        [DataMember]
+        public string FirstName;
+        [DataMember]
+        public string LastName;
+    }
+    [ServiceContract]
+    public interface IMyMath
+    {
+        [OperationContract]
+        int Add(int a, int b);
+        [OperationContract]
+        Person Write(string name, string surname);
+    }
+
+    public class MyMath : IMyMath
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+        public Person Write(string name, string surname)
+        {
+            Person p = new Person ();
+            p.FirstName = name;
+            p.LastName = surname;
+           
+            
+            //string result = "Name = " + p.FirstName+ " Last name = " + p.LastName;
+            return p;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost sh = new ServiceHost(typeof(MyMath));
+            //sh.AddServiceEndpoint(typeof(IMyMath), new WSHttpBinding(), "http://localhost:8001/MyMath/Ep1");
+           // ServiceMetadataBehavior behavior = new ServiceMetadataBehavior();
+            
+           // behavior.HttpGetEnabled = true;
+            
+            //sh.Description.Behaviors.Add(behavior);
+            
+            //sh.AddServiceEndpoint(
+            //typeof(IMetadataExchange),
+            //MetadataExchangeBindings.CreateMexHttpBinding(),
+            //"mex");
+            sh.Open();
+            Console.WriteLine("To end process press ENTER");
+            Console.ReadLine();
+            sh.Close();
+        }
+    }
+}
